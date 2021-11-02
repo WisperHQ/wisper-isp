414d9dc361a538e07b740527d8b207ee7ef0a174
Ebuka Arinze <34051115+ebukaarinze@users.noreply.github.com>
2021-11-02 20:31:57 +0100

    Edited Content

--- internet-wisper-mobile.php
+++ internet-wisper-mobile.php
@@ -240,8 +240,8 @@
             <div class="row justify-content-center">
                 <div class="col-xl-6 col-lg-8 col-md-10">
                     <div class="section-title text-center" data-animate="fadeInUp" data-delay=".1">
-                        <h2>All Plans Included</h2>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+                        <h2>How it works</h2>
+                        <p>WE KEPT THE PROCESS AS SIMPLE AS POSSIBLE</p>
                     </div>
                 </div>
             </div>
@@ -253,8 +253,8 @@
                             <img src="img/icons/ftp.svg" alt="" class="svg">
                         </div>
                         <div class="single-feature-content">
-                            <h4>Own FTP Server</h4>
-                            <p>At vero eos et accusamus et iusto dignissimosus qui blanditiis praesentium uptatum</p>
+                            <h4>Visit Website</h4>
+                            <p>Visit <a href="https://mobile.wisper.ng/buy"</a> on any of your devices.</p>
                         </div>
                     </div>
                 </div>
@@ -264,41 +264,8 @@
                             <img src="img/icons/upgrade.svg" alt="" class="svg">
                         </div>
                         <div class="single-feature-content">
-                            <h4>Upgrade or Downgrade</h4>
-                            <p>At vero eos et accusamus et iusto dignissimosus qui blanditiis praesentium uptatum</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single-feature single-feature-img-left text-left" data-animate="fadeInUp" data-delay=".35">
-                        <div class="single-feature-img">
-                            <img src="img/icons/data.svg" alt="" class="svg">
-                        </div>
-                        <div class="single-feature-content">
-                            <h4>No Hard Data Limit</h4>
-                            <p>At vero eos et accusamus et iusto dignissimosus qui blanditiis praesentium uptatum</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single-feature single-feature-img-left text-left" data-animate="fadeInUp" data-delay=".5">
-                        <div class="single-feature-img">
-                            <img src="img/icons/lock.svg" alt="" class="svg">
-                        </div>
-                        <div class="single-feature-content">
-                            <h4>Lock in Low Rates</h4>
-                            <p>At vero eos et accusamus et iusto dignissimosus qui blanditiis praesentium uptatum</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single-feature single-feature-img-left text-left" data-animate="fadeInUp" data-delay=".65">
-                        <div class="single-feature-img">
-                            <img src="img/icons/cloud.svg" alt="" class="svg">
-                        </div>
-                        <div class="single-feature-content">
-                            <h4>No Video Streaming</h4>
-                            <p>At vero eos et accusamus et iusto dignissimosus qui blanditiis praesentium uptatum</p>
+                            <h4>Network and Plans</h4>
+                            <p>Enter your Phone number, select a data plan and enter your fullname and Email Address.</p>
                         </div>
                     </div>
                 </div>
@@ -308,8 +275,8 @@
                             <img src="img/icons/access.svg" alt="" class="svg">
                         </div>
                         <div class="single-feature-content">
-                            <h4>Access to All Website</h4>
-                            <p>At vero eos et accusamus et iusto dignissimosus qui blanditiis praesentium uptatum</p>
+                            <h4>Payment and Enjoy Data</h4>
+                            <p>Make payment with your debit card at no added cost. After payment, you can start surfing the internet.</p>
                         </div>
                     </div>
                 </div>
@@ -327,7 +294,7 @@
                     <div class="faq pb-50" data-animate="fadeInUp" data-delay=".1">
                         <div class="section-title">
                             <h2>Frequently Asked Questions</h2>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some</p>
+                            <p>Can I buy Data for my current phone numbers?</p>
                         </div>
                         <div class="accordion" id="accordionFaq">
                             <div class="single-faq">
@@ -336,27 +303,27 @@
                                 </div>
                                 <div id="collapseOne" class="collapse show" data-parent="#accordionFaq">
                                     <div class="faq-answer">
-                                        <p><span>Ans: </span>I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+                                        <p><span>Ans: </span>Yes, you can. Click on this <a href="https://mobile.wisper.ng/buy">link</a> select a network and data plan, fill in your info and proceed to make payment. The internet data plan would be credited directly to your phone number.</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="single-faq">
                                 <div class="faq-title d-flex align-items-center">
-                                    <h3 class="h5" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">I already paid my bill but still its showing Due. Why is that?</h3>
+                                    <h3 class="h5" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">My network operator is not supported on the wisper network. How do I get data?</h3>
                                 </div>
                                 <div id="collapseTwo" class="collapse" data-parent="#accordionFaq">
                                     <div class="faq-answer">
-                                        <p><span>Ans: </span>Pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.</p>
+                                        <p><span>Ans: </span>If your network operator is not currently supported on the wisper virtual network, there is a high chance that we are currently working on getting them available so try wisper back in a few days.</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="single-faq">
                                 <div class="faq-title d-flex align-items-center">
-                                    <h3 class="h5" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">How can I contact with your emergency higher management?</h3>
+                                    <h3 class="h5" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">I successfully made a payment for a data plan, but I can't see the data</h3>
                                 </div>
                                 <div id="collapseThree" class="collapse" data-parent="#accordionFaq">
                                     <div class="faq-answer">
-                                        <p><span>Ans: </span>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.</p>
+                                        <p><span>Ans: </span>If you do not get an SMS from us after successful payment stating you have gotten your data, send the transaction code to our support team via live chat on our website or mail at support@wisper.ng, and the data will be credited to you asap.</p>
                                     </div>
                                 </div>
                             </div>
