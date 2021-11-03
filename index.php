04b7d4c3436d06b4c1bda3cce3ce22418d2a41c5
warriCEO <34051115+ebukaarinze@users.noreply.github.com>
2021-11-03 23:39:45 +0100

    fix

--- index.php
+++ index.php
@@ -135,8 +135,16 @@
     <!-- End of About Us -->
 
     <!-- Services -->
-    <section class="pt-120 pb-90">
+    <section class="theme-bg-overlay bg-img-md-none pt-120 pb-90" data-bg-img="img/review-bg.png" data-rjs="2">
         <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-6 col-lg-8 col-md-10">
+                    <div class="section-title section-title-white text-center" data-animate="fadeInUp" data-delay=".1">
+                        <h2>Services We Provide</h2>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form by injected humour.</p>
+                    </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-lg-3 col-sm-6">
                     <div class="single-service text-center" data-animate="fadeInUp" data-delay=".1">
