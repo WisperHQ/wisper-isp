5ec904de930ac0c350140a2698da38efde0fb73a
warriCEO <34051115+ebukaarinze@users.noreply.github.com>
2021-11-15 14:35:51 +0100

    Changes

--- internet-home-sme.php
+++ internet-home-sme.php
@@ -63,12 +63,12 @@
                         <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
                         <p><i>We denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment.</i></p>
                         <ul class="rubik list-unstyled m-0 clearfix">
-                            <li>WiFi control</li>
-                            <li>10 e-mail accounts</li>
-                            <li>Pro Installation</li>
-                            <li>Wall-to-Wall Fast WiFi</li>
-                            <li>Serviney Security</li>
-                            <li>Money back Guarantee</li>
+                            <li>Unlimited Data</li>
+                            <li>Reliable Network</li>
+                            <li>Guaranteed Speed</li>
+                            <li>Cost Efficent</li>
+                            <li>Flexibility</li>
+                            <li>24/7 Remote and Onsite Support</li>
                         </ul>
                     </div>
                 </div>
@@ -402,7 +402,7 @@
     <!-- End of FAQ -->
 
     <!-- Reviews -->
-    <!--  <?php require "reviews.php" ?> -->
+    <?php require "reviews.php" ?>
     <!-- End of Reviews -->
 
      <!-- Footer -->
